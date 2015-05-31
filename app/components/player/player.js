d6556baeee7c714756ff8ba0be5fbe43afa0aebe
Joel Tetrault <star99ers@gmail.com>
2015-05-31 14:00:47 -0500

    add back button to player detail page avoid returning "undefined undefined" for full name before player model is loaded

--- app/components/player/player.js
+++ app/components/player/player.js
@@ -7,6 +7,8 @@ var playerServices = angular.module('playerServices', ['ngResource']);
 playerServices.factory('Player', ['$resource',
   function($resource){
     var Player = $resource('/players/:playerId', {playerId: '@link_name'}, {});
-    Player.prototype.full_name = function() {return this.first_name + ' ' + this.last_name;};
+    Player.prototype.full_name = function() {
+      return (this.first_name || '') + ' ' + (this.last_name || '');
+    };
     return Player;
   }]);
